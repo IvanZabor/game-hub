48c727ae36c09de39d834fa9a7e767ae19dd4a10
izabor <ivan.zaborovets@gmail.com>
2023-07-27 22:16:05 +0300

    add genre icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,9 +13,11 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="1xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList platforms={game.platforms.map((p) => p.platform)} />
+        </HStack>
+        <HStack justifyContent="space-between">
           <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
