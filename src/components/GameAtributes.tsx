d6515397fa70cc04c90de4b14091157c29a08161
izabor <ivan.zaborovets@gmail.com>
2023-08-11 19:17:35 +0300

    done

--- src/components/GameAtributes.tsx
+++ src/components/GameAtributes.tsx
@@ -1,4 +1,4 @@
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import { SimpleGrid, Text } from "@chakra-ui/layout";
 import CriticScore from "./CriticScore";
 import DefinitionItem from "./DefinitionItem";
