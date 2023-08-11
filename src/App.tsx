469ec987a48fdc85a80f93fdbb881d3fedf6d4fb
izabor <ivan.zaborovets@gmail.com>
2023-08-11 14:11:08 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import { useState } from "react";
 import GameGrid from "./components/Game/GameGrid";
 import GameHeading from "./components/Game/GameHeading";
 import GenreList from "./components/GenreList";
@@ -7,16 +6,7 @@ import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/Platform/PlatformSelector";
 import SortSelector from "./components/SortSelector";
 
-export interface GameQuery {
-  genreId?: number;
-  platformId?: number;
-  sortOrder: String;
-  searchText: string;
-}
-
 function App() {
-  const [gameQuery, setGameQuary] = useState<GameQuery>({} as GameQuery);
-
   return (
     <Grid
       templateAreas={{
@@ -29,41 +19,24 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar
-          onSearch={(searchText) => setGameQuary({ ...gameQuery, searchText })}
-        />
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList
-            onSelectGenre={(genre) =>
-              setGameQuary({ ...gameQuery, genreId: genre.id })
-            }
-            selectedGenreId={gameQuery.genreId}
-          />
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main">
         <Box paddingLeft={10}>
-          <GameHeading gameQuary={gameQuery} />
+          <GameHeading />
           <Flex>
             <Box marginRight={5}>
-              <PlatformSelector
-                selectedPlatformId={gameQuery.platformId}
-                onSelectPlatform={(platform) =>
-                  setGameQuary({ ...gameQuery, platformId: platform.id })
-                }
-              />
+              <PlatformSelector />
             </Box>
-            <SortSelector
-              sortOrder={gameQuery.sortOrder}
-              onSelectSortOrder={(sortOrder) =>
-                setGameQuary({ ...gameQuery, sortOrder })
-              }
-            />
+            <SortSelector />
           </Flex>
         </Box>
-        <GameGrid gameQuery={gameQuery} />
+        <GameGrid />
       </GridItem>
     </Grid>
   );
--- src/components/Game/GameHeading.tsx
+++ src/components/Game/GameHeading.tsx
@@ -1,16 +1,14 @@
 import { Heading } from "@chakra-ui/react";
-import { GameQuery } from "../../App";
-import useGenres from "../../hooks/useGenres";
 import usePlatform from "../../hooks/usePlatform";
 import useGenre from "../../hooks/useGenre";
+import useGameQueryStore from "../../store";
 
-interface Props {
-  gameQuary: GameQuery;
-}
+const GameHeading = () => {
+  const genreId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const genre = useGenre(genreId);
 
-const GameHeading = ({ gameQuary }: Props) => {
-  const genre = useGenre(gameQuary.genreId);
-  const platform = usePlatform(gameQuary.platformId);
+  const platformId = useGameQueryStore((s) => s.gameQuery.platformId);
+  const platform = usePlatform(platformId);
 
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
